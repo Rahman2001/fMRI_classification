1a60f1608b982b051988c64499c0d35671502d38
Osman Nurettin Çoban <155082366+osman-1002@users.noreply.github.com>
2024-12-17 00:26:16 +0300

    finalized the BaseDataset and BNU_EOEC1Dataset classes

--- data_load_and_preprocessing/datasets.py
+++ data_load_and_preprocessing/datasets.py
@@ -1,109 +1,122 @@
 import os
 import torch
 from torch.utils.data import Dataset
-import augmentations
+import augmentations as aug
 import pandas as pd
 from pathlib import Path
+import random
 
 class BaseDataset(Dataset):
     def __init__(self):
         super().__init__()
 
-    def register_args(self, **kwargs):
-        
-        self.device = None  #torch.device('cuda') if kwargs.get('cuda') else torch.device('cpu')
-        self.index_l = []
-        self.norm = 'global_normalize'
-        self.complementary = 'per_voxel_normalize'
-        self.random_TR = kwargs.get('random_TR')
-        self.set_augmentations(**kwargs)
-        self.stride_factor = 1
-        self.sequence_stride = 1
-        self.sequence_length = kwargs.get('sequence_length')
-        self.sample_duration = self.sequence_length * self.sequence_stride
-        self.stride = max(round(self.stride_factor * self.sample_duration), 1)
-        self.TR_skips = range(0, self.sample_duration, self.sequence_stride)
+    def initialize_params(self, **config):
+        self.device = config.get('device', 'cpu')
+        self.index_data = []
+        self.global_norm = 'global_normalize'
+        self.voxel_norm = 'per_voxel_normalize'
+        self.random_timepoint = config.get('random_timepoint', False)
+        self.setup_augmentations(**config)
+        self.temporal_stride = config.get('stride_factor', 1)
+        self.sequence_step = config.get('sequence_stride', 1)
+        self.seq_length = config.get('sequence_length', 10)
+        self.duration = self.seq_length * self.sequence_step
+        self.skip_steps = list(range(0, self.duration, self.sequence_step))
 
     def get_input_shape(self):
-        shape = torch.load(os.path.join(self.index_l[0][2], self.index_l[0][3] + '.pt')).squeeze().shape
-        return shape
+        sample = torch.load(os.path.join(self.index_data[0][2], self.index_data[0][3] + '.pt'))
+        return sample.squeeze().shape
 
-    def set_augmentations(self, **kwargs):
-        if kwargs.get('augment_prob') > 0:
-            self.augment = augmentations.brain_gaussian(**kwargs)
-        else:
-            self.augment = None
+    def setup_augmentations(self, **config):
+        prob = config.get('augment_prob', 0)
+        self.augment = aug.brain_gaussian(**config) if prob > 0 else None
 
-    def TR_string(self, filename_TR, x):
-        # all datasets should have the TR mentioned in the format of 'some prefix _ number.pt'
-        TR_num = [xx for xx in filename_TR.split('_') if xx.isdigit()][0]
-        assert len(filename_TR.split('_')) == 4
-        filename = filename_TR.replace(TR_num, str(int(TR_num) + x)) + '.pt'
-        return filename
+    def get_timepoint_filename(self, base_name, offset):
+        time_idx = [part for part in base_name.split('_') if part.isdigit()][0]
+        assert len(base_name.split('_')) == 4, "Filename must follow the expected format."
+        updated_name = base_name.replace(time_idx, str(int(time_idx) + offset)) + '.pt'
+        return updated_name
 
-    def determine_TR(self, TRs_path, TR):
-        if self.random_TR:
-            possible_TRs = len(os.listdir(TRs_path)) - self.sample_duration
-            TR = 'rfMRI_r_TR_' + str(torch.randint(0, possible_TRs, (1,)).item())
-        return TR
+    def resolve_timepoint(self, base_path, timepoint):
+        if self.random_timepoint:
+            max_offset = len(os.listdir(base_path)) - self.duration
+            timepoint = f'rfMRI_r_TR_{random.randint(0, max_offset)}'
+        return timepoint
 
-    def load_sequence(self, TRs_path, TR):
-        # the logic of this function is that always the first channel corresponds to global norm and if there is a
-        # second channel it belongs to per voxel.
-        TR = self.determine_TR(TRs_path, TR)
-        y = torch.cat(
-            [torch.load(os.path.join(TRs_path, self.TR_string(TR, x)), map_location=self.device).unsqueeze(0) for x in
-             self.TR_skips], dim=4)
-        if self.complementary is not None:
-            y1 = torch.cat([torch.load(
-                os.path.join(TRs_path, self.TR_string(TR, x)).replace(self.norm, self.complementary),
-                map_location=self.device).unsqueeze(0)
-                            for x in self.TR_skips], dim=4)
-            y1[y1 != y1] = 0
-            y = torch.cat([y, y1], dim=0)
-            del y1
-        return y
+    def load_sequence_data(self, base_path, timepoint):
+        resolved_timepoint = self.resolve_timepoint(base_path, timepoint)
+        data = torch.cat([
+            torch.load(os.path.join(base_path, self.get_timepoint_filename(resolved_timepoint, step)),
+                        map_location=self.device).unsqueeze(0)
+            for step in self.skip_steps
+        ], dim=4)
+
+        if self.voxel_norm:
+            voxel_data = torch.cat([
+                torch.load(
+                    os.path.join(base_path, self.get_timepoint_filename(resolved_timepoint, step)).replace(
+                        self.global_norm, self.voxel_norm),
+                    map_location=self.device
+                ).unsqueeze(0) for step in self.skip_steps
+            ], dim=4)
+
+            voxel_data[torch.isnan(voxel_data)] = 0
+            data = torch.cat([data, voxel_data], dim=0)
+
+        return data
 
 
-class BNU_EOEC1(BaseDataset):
-    def __init__(self, **kwargs):
-        self.register_args(**kwargs)
-        self.root = r'C:\Users\Osman\Desktop\Data_Mining_2\project\datasets\Beijing_Normal_University_EOEC1'
-        self.meta_data = pd.read_csv(os.path.join(kwargs.get('base_path'), 'data', 'metadata', 'BNU_EOEC1.csv'))
-        self.data_dir = os.path.join(self.root, 'MNI_to_TRs')
-        self.subject_names = os.listdir(self.data_dir)
-        print(f'subject_names={self.subject_names}')
-        self.label_dict = {'open': torch.tensor([0.0]), 'closed': torch.tensor([1.0]),
-                           '22-25': torch.tensor([1.0, 0.0]),
-                           '26-30': torch.tensor([1.0, 0.0]),
-                           '31-35': torch.tensor([0.0, 1.0]), '36+': torch.tensor([0.0, 1.0])}  # torch.tensor([1])}
-        self.subject_folders = []
-        for i, subject in enumerate(os.listdir(self.data_dir)):
+class BNU_EOEC1Dataset(BaseDataset):
+    def __init__(self, **config):
+        self.initialize_params(**config)
+        self.dataset_path = Path(config.get('dataset_path', './datasets/BNU_EOEC1'))
+        self.metadata_file = Path(config.get('base_path')) / 'data' / 'metadata' / 'BNU_EOEC1.csv'
+        self.data_directory = self.dataset_path / 'MNI_to_TRs'
+        self.meta_info = pd.read_csv(self.metadata_file)
+        self.subjects = os.listdir(self.data_directory)
+        self.label_mapping = {
+            'open': torch.tensor([0.0]),
+            'closed': torch.tensor([1.0]),
+            '22-25': torch.tensor([1.0, 0.0]),
+            '26-30': torch.tensor([1.0, 0.0]),
+            '31-35': torch.tensor([0.0, 1.0]),
+            '36+': torch.tensor([0.0, 1.0])
+        }
+        self._prepare_indices()
+
+    def _prepare_indices(self):
+        for idx, subject in enumerate(self.subjects):
             try:
-                age = torch.tensor(
-                    self.meta_data[self.meta_data['SUBID_SESSION'] == int(subject)]['AGE'].values[0])
-            except Exception:
-                print("Something is wrong with values in dataset in following attributes (subject_id or age)")
+                subject_metadata = self.meta_info[self.meta_info['SUBID_SESSION'] == int(subject)]
+                age = torch.tensor(subject_metadata['AGE'].values[0])
+                eye_status = subject_metadata['EYESTATUS'].values[0]
+                tr_path = self.data_directory / subject / self.global_norm
 
-            eoec = self.meta_data[self.meta_data['SUBID_SESSION'] == int(subject)]['EYESTATUS'].values[0]
-            path_to_TRs = os.path.join(self.data_dir, subject, self.norm)
-            subject_duration = len(os.listdir(path_to_TRs))  # 121
-            session_duration = subject_duration - self.sample_duration
-            filename = os.listdir(path_to_TRs)[0]
-            filename = filename[:filename.find('TR') + 3]
+                duration = len(os.listdir(tr_path))
+                valid_duration = duration - self.duration
+                tr_filename = os.listdir(tr_path)[0].split('_TR')[0]
 
-            for k in range(0, session_duration, self.stride):
-                self.index_l.append((i, subject, path_to_TRs, filename + str(k), session_duration, age, eoec))
+                for start in range(0, valid_duration, self.temporal_stride):
+                    self.index_data.append((idx, subject, str(tr_path), f"{tr_filename}_TR_{start}", valid_duration, age, eye_status))
+
+            except KeyError as e:
+                print(f"Error processing subject {subject}: {e}")
 
     def __len__(self):
-        N = len(self.index_l)
-        return N
+        return len(self.index_data)
 
     def __getitem__(self, index):
-        subj, subj_name, path_to_TRs, TR, session_duration, age, eoec = self.index_l[index]
-        age = self.label_dict[age] if isinstance(age, str) else age.float()
-        y = self.load_sequence(path_to_TRs, TR)
-        if self.augment is not None:
-            y = self.augment(y)
-        return {'fmri_sequence': y, 'subject': subj, 'subject_binary_classification': self.label_dict[eoec],
-                'subject_regression': age, 'TR': int(TR.split('_')[-1])}
+        subj_idx, subj_id, tr_path, tr_name, duration, age, eye_status = self.index_data[index]
+        age_label = self.label_mapping.get(str(age), age.float())
+        sequence_data = self.load_sequence_data(tr_path, tr_name)
+
+        if self.augment:
+            sequence_data = self.augment(sequence_data)
+
+        return {
+            'fmri_sequence': sequence_data,
+            'subject': subj_idx,
+            'subject_binary_classification': self.label_mapping[eye_status],
+            'subject_regression': age_label,
+            'TR': int(tr_name.split('_')[-1])
+        }
